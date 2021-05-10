71b427e46f0df5095e9e317d12dc848ea4825a10
Rafael Malcervelli <48256253+MalcerOne@users.noreply.github.com>
2021-05-10 13:16:46 -0300

    corrigir modulação e perguntar sobre fourier

--- Projeto8/funcoes_LPF.py
+++ Projeto8/funcoes_LPF.py
@@ -2,6 +2,7 @@ from scipy.fftpack import fft, next_fast_len
 from scipy import signal as window
 import numpy as np
 import scipy.signal as sg
+import math
 
 def filtro(y, samplerate, cutoff_hz):
   # https://scipy.github.io/old-wiki/pages/Cookbook/FIRFilter.html
@@ -13,20 +14,40 @@ def filtro(y, samplerate, cutoff_hz):
     yFiltrado = sg.lfilter(taps, 1.0, y)
     return yFiltrado
 
-def LPF(signal, cutoff_hz, fs):
-        from scipy import signal as sg
-        #####################
-        # Filtro
-        #####################
-        # https://scipy.github.io/old-wiki/pages/Cookbook/FIRFilter.html
-        nyq_rate = fs/2
-        width = 5.0/nyq_rate
-        ripple_db = 60#dB
-        N , beta = sg.kaiserord(ripple_db, width)
-        taps = sg.firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-        return( sg.lfilter(taps, 1.0, signal))
+# def LPF(signal, cutoff_hz, fs):
+#         from scipy import signal as sg
+#         #####################
+#         # Filtro
+#         #####################
+#         # https://scipy.github.io/old-wiki/pages/Cookbook/FIRFilter.html
+#         nyq_rate = fs/2
+#         width = 5.0/nyq_rate
+#         ripple_db = 60#dB
+#         N , beta = sg.kaiserord(ripple_db, width)
+#         taps = sg.firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
+#         return( sg.lfilter(taps, 1.0, signal))
 
-def calcFFT(self, signal, fs):
+# def butter_lowpass(cutoff, fs, order=5):
+#     nyq = 0.5 * fs
+#     normal_cutoff = cutoff / nyq
+#     b, a = sg.butter(order, normal_cutoff, btype='low', analog=False)
+#     return b, a
+
+# def butter_lowpass_filter(data, cutoff, fs, order=5):
+#     b, a = butter_lowpass(cutoff, fs, order=order)
+#     y = sg.lfilter(b, a, data)
+#     return y
+
+def low_pass_filter(samplerate, cutoff, audioNormalizado):
+        # https://stackoverflow.com/questions/24920346/filtering-a-wav-file-using-python
+        freqRatio = cutoff/samplerate
+        N = int(math.sqrt(0.196201 + freqRatio**2) / freqRatio)
+        win = np.ones(N)
+        win *= 1.0/N
+        return sg.lfilter(win, [1], audioNormalizado)
+
+
+def calcFFT(signal, fs):
         # https://docs.scipy.org/doc/scipy/reference/tutorial/fftpack.html
         N  = len(signal)
         W = window.hamming(N)
--- Projeto8/projeto8.py
+++ Projeto8/projeto8.py
@@ -6,6 +6,7 @@ from scipy.io import wavfile
 from pyfiglet import Figlet
 import scipy.signal as sps
 import sounddevice as sd
+import matplotlib.pyplot as plt
 
 def main():
     f1 = Figlet(font='slant')
@@ -40,18 +41,79 @@ def main():
     
     ## Filtrar as altas frequências
     print(f"\n[+]---Filtrando altas frequências do audio\n")
-    audioFiltered = LPF(audioNormalizado, 4000, samplerate)
+    audioFiltered = low_pass_filter(samplerate, 4000, audioNormalizado)
 
     ## Reproduzir o sinal (opaco)
     print(f"\n[+]---Tocando o audio com altas frequências filtradas\n")
-    sd.play(audioFiltered, samplerate)
+    sd.play(audioFiltered)
     sd.wait()
-    ## Modular o sinal em AM com Carrierde 20 MHz
-    ## Reproduzir o sinal (não é perfeitamente audível)
-    ## Gráfico Fourier x Tempo do áudio original
-    ## Gráfico Fourier x Tempo do áudio normalizado
-    ## Gráfico Fourier x Tempo do áudio filtrado
-    ## Gráfico Fourier x Tempo do áudio modulado
 
+    ## Modular o sinal em AM com Carrier de 20 MHz
+    print(f"\n[+]---Modulando o sinal por AM de 20 MHz\n")
+    t = np.linspace(0, duration, number_of_samples)
+    carrier = 1*np.cos(2*np.pi*20000*t)
+    audioModAM = np.dot(carrier, audioFiltered)
+
+    ## Reproduzir o sinal (não é perfeitamente audível)
+    print(f"\n[+]---Tocando o audio modulado (não é audível)\n")
+    print(audioModAM)
+    sd.play(audioModAM)
+    sd.wait()
+
+    ## Gráfico Fourier x Tempo do áudio original
+    arrayAudioOriginal = np.ndarray(shape=(88200,),dtype=np.float32)
+    for i in range(0,arrayAudioOriginal.shape[0]):
+        arrayAudioOriginal[i] = audio[i][0]
+
+    tempo = np.linspace(0, duration, 44100)
+    xf, yf = calcFFT(arrayAudioOriginal, 44100)
+    plt.plot(xf, yf, label="Áudio Original")
+    plt.title("Fourier do áudio original")
+    plt.xlabel("Frequências")
+    plt.legend()
+    plt.savefig("FourierAudioOriginal.jpg")
+    plt.show()
+
+    ## Gráfico Fourier x Tempo do áudio normalizado
+    arrayAudioNormalizado = np.ndarray(shape=(88200,),dtype=np.float32)
+    for i in range(0,arrayAudioNormalizado.shape[0]):
+        arrayAudioNormalizado[i] = audioNormalizado[i][0]
+
+    tempo = np.linspace(0, duration, 44100)
+    xf, yf = calcFFT(arrayAudioNormalizado, 44100)
+    plt.plot(xf, yf, label="Áudio Normalizado")
+    plt.title("Fourier do áudio normalizado")
+    plt.xlabel("Frequências")
+    plt.legend()
+    plt.savefig("FourierAudioNormalizado.jpg")
+    plt.show()
+
+    ## Gráfico Fourier x Tempo do áudio filtrado
+    arrayAudioFiltered = np.ndarray(shape=(88200,),dtype=np.float32)
+    for i in range(0,arrayAudioFiltered.shape[0]):
+        arrayAudioFiltered[i] = audioFiltered[i][0]
+
+    tempo = np.linspace(0, duration, 44100)
+    xf, yf = calcFFT(arrayAudioFiltered, 44100)
+    plt.plot(xf, yf, label="Áudio Filtrado")
+    plt.title("Fourier do áudio filtrado")
+    plt.xlabel("Frequências")
+    plt.legend()
+    plt.savefig("FourierAudioFiltrado.jpg")
+    plt.show()
+
+    ## Gráfico Fourier x Tempo do áudio modulado
+    arrayAudioModAM = np.ndarray(shape=(88200,),dtype=np.float32)
+    for i in range(0,arrayAudioModAM.shape[0]):
+        arrayAudioModAM[i] = audioModAM[i][0]
+
+    tempo = np.linspace(0, duration, 44100)
+    xf, yf = calcFFT(arrayAudioModAM, 44100)
+    plt.plot(xf, yf, label="Áudio Modulado")
+    plt.title("Fourier do áudio modulado")
+    plt.xlabel("Frequências")
+    plt.legend()
+    plt.savefig("FourierAudioModulado.jpg")
+    plt.show()
 if __name__ == "__main__":
     main()
